4d2a15bebfc11187081648784ec3cd6f5480baa0
Frederic KOLLER <koller.f@gmail.com>
2018-12-04 16:32:14 +0100

    Add optionnal on click

--- js/ripple.js
+++ js/ripple.js
@@ -1,12 +1,14 @@
 function fk_ripple(event) {
     event.stopPropagation();
     var target = event.currentTarget || event.target;
+    var className = 'fk_ripple';
     var color = target.getAttribute('data-fk-ripple-color'),
         tH, tW, sizeSquare, span;
 
-    if (target.querySelectorAll('.ripple').length) {
-        return false;
-    }
+    // On click only
+    // if (target.querySelectorAll('.' + className).length) {
+    //     return false;
+    // }
 
     // Get sizes on clicked element
     tH = target.clientHeight;
@@ -17,7 +19,7 @@ function fk_ripple(event) {
 
     // Create SPAN element
     span = document.createElement('span');
-    span.classList.add('fk_ripple');
+    span.classList.add(className);
     span.style.left = (event.offsetX - (sizeSquare / 2)) + 'px';
     span.style.top = (event.offsetY - (sizeSquare / 2)) + 'px';
     span.style.width = sizeSquare + 'px';
